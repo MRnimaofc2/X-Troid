ff1d9613b29e3060dfccc6a76df872400bd1d1f7
ᴍʀ ɴɪᴍᴀ 2ɴᴅ ɢɪᴛ <103928995+MRnimaofc2@users.noreply.github.com>
2022-05-02 10:38:52 +0530

    🐺 x-ᴛʀᴏɪᴅ ɴɪᴍᴀ 🐺

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -960,12 +960,12 @@ else if (config.WORKTYPE == 'public') {
 
         let title = arama[0].title.replace(' ', '+');
         let stream = ytdl(arama[0].videoId, {
-            quality: 'normalaudio',
+            quality: 'highestaudio',
         });
     
         got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
         ffmpeg(stream)
-            .audioBitrate(128)
+            .audioBitrate(320)
             .save('./' + title + '.mp3')
             .on('end', async () => {
                 const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
